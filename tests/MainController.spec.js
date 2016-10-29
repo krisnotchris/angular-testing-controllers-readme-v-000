6be667bb93e29f232c0688ac085320e7896d53c4
krisnotchris <krishenderson@gmail.com>
2016-10-29 12:53:48 -0500

    Done.

--- tests/MainController.spec.js
+++ tests/MainController.spec.js
@@ -6,4 +6,11 @@ describe('MainController', function() {
   beforeEach(inject(function(_$controller_){
     $controller = _$controller_;
   }));
+ 
+  it('should have Steve Jobs name', function() {
+    var $scope = {};
+ 
+    var controller = $controller('MainController', { $scope: $scope });
+    expect(controller.name).toEqual('Steve Jobs');
+  });
 });
